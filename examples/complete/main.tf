f34db9121bd621dec5a0811830781abcc8736d64
scotttyso <tyscott@cisco.com>
2022-09-25 01:32:41 -0400

    new TF 1.3 and organization moid option

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,10 +2,10 @@ module "iscsi_adapter" {
   source  = "terraform-cisco-modules/policies-iscsi-adapter/intersight"
   version = ">= 1.0.1"
 
-  description      = "default iSCSI Adapter Policy."
-      dhcp_timeout           = 60
-  name         = "default"
-      lun_busy_retry_count   = 15
-  organization = "default"
-      tcp_connection_timeout = 15
+  description            = "default iSCSI Adapter Policy."
+  dhcp_timeout           = 60
+  name                   = "default"
+  lun_busy_retry_count   = 15
+  organization           = "default"
+  tcp_connection_timeout = 15
 }
--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@
 #____________________________________________________________
 
 data "intersight_organization_organization" "org_moid" {
-  name = var.organization
+  for_each = {
+    for v in [var.organization] : v => v if length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+    ) == 0
+  }
+  name = each.value
 }
 
 #__________________________________________________________________
@@ -24,7 +29,11 @@ resource "intersight_vnic_iscsi_adapter_policy" "iscsi_adapter" {
   lun_busy_retry_count = var.lun_busy_retry_count
   name                 = var.name
   organization {
-    moid        = data.intersight_organization_organization.org_moid.results[0].moid
+    moid = length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+      ) > 0 ? var.organization : data.intersight_organization_organization.org_moid[
+      var.organization].results[0
+    ].moid
     object_type = "organization.Organization"
   }
   dynamic "tags" {
